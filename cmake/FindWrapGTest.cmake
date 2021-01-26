bef2774dd64c2d43b060f95ac17321e3b36ea21f
Alexey Edelev <semlanik@gmail.com>
2021-01-26 21:01:39 +0100

    Update build procedure
    - Adjust build procedure to the one implemented in Qt6
    - Decided to make QtProtobuf and QtGrpc a single module in the
      end. Root CMakeLists.txt supposed to find all dependencies for
      the module and if optional depenencies are not found, some
      features will be disabled. This is how it's designed in Qt6.
    - Add Protobuf, gRPC and GTest wrapper modules. In-source build
      of gRPC is too expensive and looks redundand for me. So it's
      deprecated and will be removed in futher versions.

---
+++ cmake/FindWrapGTest.cmake
@@ -0,0 +1,15 @@
+cmake_minimum_required(VERSION 3.10)
+
+find_package(GTest QUIET)
+
+unset(WrapGTest_FOUND)
+if(NOT GTest_FOUND)
+    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/googletest/CMakeLists.txt")
+        set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+        set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+        add_subdirectory("3rdparty/googletest" EXCLUDE_FROM_ALL)
+        set(WrapGTest_FOUND TRUE)
+    endif()
+else()
+    set(WrapGTest_FOUND TRUE)
+endif()
